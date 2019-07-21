d33ea9e479aa6343a18b5144835943b630cd1220
Jeremy Jersin <jljersin@comcast.net>
2019-07-21 11:14:29 -0600

    fixed number generated to integer

--- guess-the-number/guess-number.py
+++ guess-the-number/guess-number.py
@@ -2,8 +2,15 @@
 import random
 
 #generate a random from 20 to 50 in step 1
-number = random.randrange(20,50,1)
+number = int(random.randrange(1,2))
 
 #get user input
 print("Program has user guess a number that is generated randomly")
-guess = input("Enter your guess: ")
+guess = int(input("Enter another guess: "))
+
+while guess != number:
+    print("Not the correct number")
+    guess = int(input("Enter another guess: "))
+
+if guess == number:
+    print("you guessed the right number")

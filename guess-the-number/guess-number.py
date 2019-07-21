adb7d69d280599c1fcd541e1bb78337d8b927024
Jeremy Jersin <jljersin@comcast.net>
2019-07-21 10:24:39 -0600

    added user input

--- guess-the-number/guess-number.py
+++ guess-the-number/guess-number.py
@@ -1,5 +1,9 @@
 #import random library
 import random
 
-#generate a random from 20 to 50 in step 3
+#generate a random from 20 to 50 in step 1
 number = random.randrange(20,50,1)
+
+#get user input
+print("Program has user guess a number that is generated randomly")
+guess = input("Enter your guess: ")

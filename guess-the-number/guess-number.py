d324c9dc314e63c6a7451a58454633256d04076d
Jeremy Jersin <jljersin@comcast.net>
2019-07-21 11:27:44 -0600

    moved into function

--- guess-the-number/guess-number.py
+++ guess-the-number/guess-number.py
@@ -1,16 +1,24 @@
-#import random library
-import random
 
-#generate a random from 20 to 50 in step 1
-number = int(random.randrange(1,2))
+def guessNumber():
+    #import random library
+    import random
 
-#get user input
-print("Program has user guess a number that is generated randomly")
-guess = int(input("Enter another guess: "))
-
-while guess != number:
-    print("Not the correct number")
+    #generate a random from 20 to 50 in step 1
+    number = int(random.randrange(1,4))
+    #get user input
+    print("Program has user guess a number that is generated randomly")
     guess = int(input("Enter another guess: "))
 
-if guess == number:
-    print("you guessed the right number")
+    while guess != number:
+
+        if guess < number:
+            print("Guess is too low")
+        elif guess > number:
+            print("Guess is too high")
+
+        guess = int(input("Enter another guess: "))
+
+    if guess == number:
+        print("You guessed the right number")
+
+guessNumber()

44821dd7c374828f0153038169c9416feb3ec25c
nerau12 <jljersin@comcast.net>
2021-07-14 13:33:54 -0600

    changed files

--- guess-the-number/guess_number_gui.py
+++ guess-the-number/guess_number_gui.py
@@ -7,13 +7,13 @@ class GuessNumber:
         self.main_window = tinker.Tk()
         self.main_window.title('Guess The Number')
         self.main_window.geometry('500x500')
-        self.create_widgets(self.main_window)
-
-    def create_widgets(self,master):
-        self.menu = tinker.Menu(master=master)
+        self.menu = tinker.Menu(self.main_window)
         self.file_menu = tinker.Menu(self.menu, tearoff=0)
-        self.file_menu.add_command(label='Exit',command= master.quit)
-        master.config(menu=self.menu)
+        self.file_menu.add_command(label="Exit",command= self.main_window.quit)
+        self.main_window.config(menu=self.menu)
+
+    def create_widgets(self):
+        pass
 
     def run_application(self):
         self.main_window.mainloop()

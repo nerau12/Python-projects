b907dc12024a4a4fb06efa29312a96bb667d0279
nerau12 <jljersin@comcast.net>
2021-07-14 15:04:26 -0600

    changes

--- guess-the-number/guess_number.py
+++ guess-the-number/guess_number.py
@@ -6,7 +6,7 @@ class Guess_Number:
     def __init__(self):
         #generate a random from 1 to 10
         self.number = int(randrange(1,100))
-        self.guess = None
+        self.guess = 0
         self.num_of_guesses = 0
         self.max_num_of_guess = 5
     
@@ -22,8 +22,10 @@ class Guess_Number:
                 print("Guess is too low")
             elif self.guess > self.number:
                 print("Guess is too high")
-
+            
+            #tell user how many guesses left
             print('You have ' + str(self.max_num_of_guess - self.num_of_guesses) + ' left')
+
             #get user input on wrong guess
             self.get_number()
         

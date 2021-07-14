b6698564f9e9fb2256f6a2445512f221afb69b92
nerau12 <jljersin@comcast.net>
2021-07-14 12:03:06 -0600

    changes

--- guess-the-number/guess_number_gui.py
+++ guess-the-number/guess_number_gui.py
@@ -1,3 +1,15 @@
-from guess-number import guessNumber
-import tkinter
+import tkinter as tinker
+from guess_number import guessNumber
 
+class GuessNumber:
+    
+    def __init__(self):
+        self.main_window = tinker.Tk()
+
+    def run_application(self):
+        self.main_window.mainloop()
+
+
+if __name__ == '__main__':
+    window = GuessNumber()
+    window.run_application()
